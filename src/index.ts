86fd4bed0154ec4c0f05a45b97f4982e911d0f20
Olivia Dang <olivia@alembic.com.au>
2022-01-22 17:31:44 +1100

    installed redis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata'
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
@@ -8,14 +8,31 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
-import { UserResolver } from './resolvers/user';
+import { UserResolver } from "./resolvers/user";
+
+const { createClient } = require("redis");
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
- 
+
+  const RedisStore = connectRedis(session);
+  const redisClient = createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient }),
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
